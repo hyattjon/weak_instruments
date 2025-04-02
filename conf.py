739d7e4ed808df739a91afb611ba11bf9397c041
hyattjon <hyatt.jonathan99@gmail.com>
2025-04-01 20:44:07 -0600

    Added basics of sphinx so that documentation creates itself as we use the docstrings

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,3 +26,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+
+import os 
+import sys
+sys.path.insert(0, os.path.abspath('.'))
